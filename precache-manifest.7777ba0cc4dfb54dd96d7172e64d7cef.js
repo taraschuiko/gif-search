5e54a27b2b2f302152671227e913e053562cbe64
taraschuiko <taraschuiko@gmail.com>
2019-03-20 21:07:53 +0200

    Updates

---
+++ precache-manifest.7777ba0cc4dfb54dd96d7172e64d7cef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gif-search/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "50e6eec20fb9d0904e17",
+    "url": "/gif-search/static/js/runtime~main.8a46e744.js"
+  },
+  {
+    "revision": "dcf139b4f531994446a6",
+    "url": "/gif-search/static/js/main.869ac53b.chunk.js"
+  },
+  {
+    "revision": "b9633c071433248c878b",
+    "url": "/gif-search/static/js/2.03f09cfb.chunk.js"
+  },
+  {
+    "revision": "dcf139b4f531994446a6",
+    "url": "/gif-search/static/css/main.81103276.chunk.css"
+  },
+  {
+    "revision": "3720e0856d5d38ce50d2260dd53dafe6",
+    "url": "/gif-search/index.html"
+  }
+];
